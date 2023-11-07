e7b197184c7be957e793511f4cb6970ab6d5ac32
heath <duweihengmr@163.com>
2023-11-07 17:15:13 +0800

    feat: add theme

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+import {BrowserRouter as Router} from "react-router-dom";
+import {ConfigProvider} from "antd";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <Router>
+            <ConfigProvider theme={
+                {
+                    components: {
+                        Message: {
+                            contentBg: '#d9d9d9'
+                        }
+                    }
+                }
+            }>
+                <App/>
+            </ConfigProvider>
+        </Router>
+    </React.StrictMode>,
 );
