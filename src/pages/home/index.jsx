a1857f7eb8481659312971ed4d6af9f5dc65f22e
heath <duweihengmr@163.com>
2023-11-07 17:13:19 +0800

    feat: add home page

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./index.css";
+import TunnelListItem from "../../components/TunnelListItem/index.jsx";
+
+
+export default function Home() {
+    return (
+        <div className="home-container">
+            <div className="left-container">
+                <div style={{margin: "5px 25px"}}>
+                    <div>内网穿透（2）</div>
+                    <div>
+
+                    </div>
+                </div>
+            </div>
+            <div className="right-container">2</div>
+        </div>
+    );
+}
