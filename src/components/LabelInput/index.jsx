ed68d41278e75f1b5c4f2751f2540a8a946a1514
heath <duweihengmr@163.com>
2023-11-08 20:11:41 +0800

    feat: add LabelInput component

---
+++ src/components/LabelInput/index.jsx
@@ -0,0 +1,31 @@
+import "./index.css"
+import {useEffect, useState} from "react";
+
+export default function LabelInput({children, value, onChange, readOnly = true}) {
+    const [txtValue, setTxtTxtValue] = useState(value)
+    useEffect(() => {
+        setTxtTxtValue(value);
+    }, [value]);
+    return (
+        <div style={{display: 'flex', marginTop: 10}}>
+            <div style={{width: 70}}>{children}</div>
+            <div style={{flexGrow: 1}}>
+                {readOnly ?
+                    <div>{txtValue}</div> :
+                    <input
+                        className="text-input"
+                        value={txtValue}
+                        onChange={(e) => {
+                            setTxtTxtValue(e.target.value)
+                            if (onChange) onChange()
+                        }}
+                    ></input>
+                }
+            </div>
+        </div>
+    )
+}
+
+LabelInput.defaultProps = {
+    type: "text",
+};
