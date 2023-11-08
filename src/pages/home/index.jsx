2260de578ade8d601b89ecb58aa0ab68b3b841d9
heath <duweihengmr@163.com>
2023-11-08 20:38:48 +0800

    feat: modify home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,20 +1,42 @@
 import React from "react";
 import "./index.css";
 import TunnelListItem from "../../components/TunnelListItem/index.jsx";
+import BorderButton from "../../components/BorderButton/index.jsx";
+import LabelInput from "../../components/LabelInput/index.jsx";
 
 
 export default function Home() {
     return (
         <div className="home-container">
             <div className="left-container">
-                <div style={{margin: "5px 25px"}}>
+                <div style={{margin: "15px 10px 15px 20px"}}>
                     <div>内网穿透（2）</div>
-                    <div>
-
+                    <div style={{overflowY: 'scroll', height: 360}}>
+                        <TunnelListItem></TunnelListItem>
+                        <TunnelListItem></TunnelListItem>
+                        <TunnelListItem></TunnelListItem>
+                        <TunnelListItem></TunnelListItem>
+                        <TunnelListItem></TunnelListItem>
+                        <TunnelListItem></TunnelListItem>
                     </div>
                 </div>
             </div>
-            <div className="right-container">2</div>
+            <div className="right-container">
+                <div style={{margin: "15px 20px"}}>
+                    <div style={{display: "flex"}}>
+                        <BorderButton style={{marginRight: 12}}>保存</BorderButton>
+                        <BorderButton>删除</BorderButton>
+                    </div>
+                    <div style={{marginTop: 10}}>
+                        <LabelInput value="192.123.12.1">外网IP：</LabelInput>
+                        <LabelInput value="999">外网端口：</LabelInput>
+                        <LabelInput value="TCP">类型:</LabelInput>
+                        <LabelInput value="工单服务服务" readOnly={false}>服务名称：</LabelInput>
+                        <LabelInput value="127.0.0.1" readOnly={false}>内网IP：</LabelInput>
+                        <LabelInput value="8080" readOnly={false}>内网端口：</LabelInput>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
