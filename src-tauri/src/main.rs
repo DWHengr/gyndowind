37ef998f6e82a6711242e246d4702a4d00810949
heath <duweihengmr@163.com>
2023-11-09 17:05:55 +0800

    feat: system tray

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,12 +2,12 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
 
-use tauri::Manager;
+use tauri::{
+    AppHandle, CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu,
+    SystemTrayMenuItem, Wry,
+};
+
 #[derive(Clone, serde::Serialize)]
 struct Payload {
     args: Vec<String>,
@@ -23,7 +23,52 @@ fn main() {
             app.emit_all("single-instance", Payload { args: argv, cwd })
                 .unwrap();
         }))
-        .invoke_handler(tauri::generate_handler![greet])
+        .system_tray(SystemTray::new().with_menu(tray_menu()))
+        .on_system_tray_event(system_tray_event)
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+// tray menu
+fn tray_menu() -> SystemTrayMenu {
+    let quit = CustomMenuItem::new("quit".to_string(), "退出");
+    let hide = CustomMenuItem::new("hide".to_string(), "隐藏");
+    let show = CustomMenuItem::new("show".to_string(), "显示");
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(hide)
+        .add_item(show)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(quit);
+    tray_menu
+}
+
+// system tray event fn
+fn system_tray_event(app: &AppHandle<Wry>, e: SystemTrayEvent) {
+    match e {
+        SystemTrayEvent::LeftClick {
+            position: _,
+            size: _,
+            ..
+        } => {
+            let window = app.get_window("main").unwrap();
+            window.show().unwrap();
+            window.set_focus().unwrap();
+        }
+        SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+            "quit" => {
+                std::process::exit(0);
+            }
+            "hide" => {
+                let window = app.get_window("main").unwrap();
+                window.hide().unwrap();
+            }
+            "show" => {
+                let window = app.get_window("main").unwrap();
+                window.show().unwrap();
+                window.set_focus().unwrap();
+            }
+            _ => {}
+        },
+        _ => {}
+    }
 }
