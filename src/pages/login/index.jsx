be64d0b7fffcf199202bdc60a840ff1a58c0cc6b
heath <duweihengmr@163.com>
2023-11-09 21:18:56 +0800

    feat: modify login page

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,7 +1,17 @@
+import "./index.css"
+import BorderInput from "../../components/BorderInput/index.jsx";
+import {useHistory} from "react-router";
+import FullButton from "../../components/FullButton/index.jsx";
+
 export default function Login() {
+
+    const h = useHistory();
+
     return (
-        <>
-            <div>登录</div>
-        </>
+        <div className="login-container">
+            <BorderInput placeholder="用户名"/>
+            <BorderInput placeholder="密码" type="password"/>
+            <FullButton onClick={() => h.push("/home")}>登 录</FullButton>
+        </div>
     )
 }
