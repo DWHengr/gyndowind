b365f449db8bdb488c3cd75e7aac78b90e99341e
heath <duweihengmr@163.com>
2023-11-13 19:39:47 +0800

    feat: modify main

--- src/main.jsx
+++ src/main.jsx
@@ -5,24 +5,28 @@ import "./styles.css";
 import {BrowserRouter as Router} from "react-router-dom";
 import {ConfigProvider} from "antd";
 import {appWindow} from "@tauri-apps/api/window";
+import store from "./store/index.jsx";
+import {Provider} from "react-redux";
 
 //居中
 await appWindow.center();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <Router>
-            <ConfigProvider theme={
-                {
-                    components: {
-                        Message: {
-                            contentBg: '#d9d9d9'
+        <Provider store={store}>
+            <Router>
+                <ConfigProvider theme={
+                    {
+                        components: {
+                            Message: {
+                                contentBg: '#d9d9d9'
+                            }
                         }
                     }
-                }
-            }>
-                <App/>
-            </ConfigProvider>
-        </Router>
+                }>
+                    <App/>
+                </ConfigProvider>
+            </Router>
+        </Provider>
     </React.StrictMode>,
 );
