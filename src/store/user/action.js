c3f1ef67052556c44e9fc0ac8d9c174cd06dd00f
heath <duweihengmr@163.com>
2023-11-14 20:44:11 +0800

    feat: modify user store

--- src/store/user/action.js
+++ src/store/user/action.js
@@ -1,28 +1,25 @@
 import * as type from "./type";
-export const setUser = (token, username, avatar) => {
-  return {
-    type: type.Set_Token,
-    token: token,
-    username: username,
-    avatar: avatar,
-  };
+
+export const setUser = (token, username, avatar, serviceIp) => {
+    return {
+        type: type.Set_Token, token: token, username: username, avatar: avatar, serviceIp: serviceIp
+    };
 };
 
 export const initUser = () => {
-  return {
-    type: type.Init_User,
-  };
+    return {
+        type: type.Init_User,
+    };
 };
 
 export const clearUser = () => {
-  return {
-    type: type.Clear_User,
-  };
+    return {
+        type: type.Clear_User,
+    };
 };
 
 export const setUserAvatar = (avatar) => {
-  return {
-    type: type.Set_Avatar,
-    avatar: avatar,
-  };
+    return {
+        type: type.Set_Avatar, avatar: avatar,
+    };
 };
--- src/store/user/reducer.js
+++ src/store/user/reducer.js
@@ -1,43 +1,43 @@
 import * as type from "./type";
 
 let defaultState = {
-  token: "",
-  isLogin: false,
-  username: "",
-  avatar: "",
+    token: "", isLogin: false, username: "", avatar: "", serviceIp: "",
 };
 
 export const userData = (state = defaultState, action) => {
-  switch (action.type) {
-    case type.Init_User:
-      let tokenData = localStorage.getItem("token");
-      let usernameDate = localStorage.getItem("username");
-      let avatarDate = localStorage.getItem("avatar");
-      let isLoginData = false;
-      if (tokenData) isLoginData = true;
-      return {
-        ...state,
-        ...{
-          token: tokenData,
-          isLogin: isLoginData,
-          username: usernameDate,
-          avatar: avatarDate,
-        },
-      };
-    case type.Set_Token:
-      localStorage.setItem("token", action.token);
-      localStorage.setItem("username", action.username);
-      localStorage.setItem("avatar", action.avatar);
-      return { ...state, ...action, ...{ isLogin: true } };
-    case type.Clear_User:
-      localStorage.removeItem("token");
-      localStorage.removeItem("username");
-      localStorage.removeItem("avatar");
-      return { ...state, ...{ token: "", isLogin: false, avatar: "" } };
-    case type.Set_Avatar:
-      localStorage.setItem("avatar", action.avatar);
-      return { ...state, ...{ avatar: action.avatar } };
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case type.Init_User:
+            let tokenData = localStorage.getItem("token");
+            let usernameDate = localStorage.getItem("username");
+            let avatarDate = localStorage.getItem("avatar");
+            let serviceIpData = localStorage.getItem("serviceIp");
+            let isLoginData = false;
+            if (tokenData) isLoginData = true;
+            return {
+                ...state, ...{
+                    token: tokenData,
+                    isLogin: isLoginData,
+                    username: usernameDate,
+                    avatar: avatarDate,
+                    serviceIp: serviceIpData
+                },
+            };
+        case type.Set_Token:
+            localStorage.setItem("token", action.token);
+            localStorage.setItem("username", action.username);
+            localStorage.setItem("avatar", action.avatar);
+            localStorage.setItem("serviceIp", action.serviceIp);
+            return {...state, ...action, ...{isLogin: true}};
+        case type.Clear_User:
+            localStorage.removeItem("token");
+            localStorage.removeItem("username");
+            localStorage.removeItem("avatar");
+            localStorage.removeItem("serviceIp");
+            return {...state, ...{token: "", isLogin: false, avatar: ""}};
+        case type.Set_Avatar:
+            localStorage.setItem("avatar", action.avatar);
+            return {...state, ...{avatar: action.avatar}};
+        default:
+            return state;
+    }
 };
