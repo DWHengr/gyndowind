43b3c26a5a7aafa4968630d88d27cde4c8b6edf6
heath <duweihengmr@163.com>
2023-11-14 20:45:23 +0800

    feat: modify FullButton component

--- src/components/FullButton/index.jsx
+++ src/components/FullButton/index.jsx
@@ -2,12 +2,14 @@ import "./index.css"
 
 export default function FullButton({children, onClick}) {
     return (
-        <div className="full-button-container">
+        <div
+            className="full-button-container"
+            onClick={() => {
+                if (onClick) onClick()
+            }}
+        >
             <div
                 style={{fontSize: 20}}
-                onClick={() => {
-                    if (onClick) onClick()
-                }}
             >
                 {children}
             </div>
