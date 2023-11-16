7f4b6c6b53d29e0e79ad276203ec7cc0aabbaddf
heath <duweihengmr@163.com>
2023-11-16 20:21:02 +0800

    feat: modify TunnelList

--- src/components/TunnelList/index.jsx
+++ src/components/TunnelList/index.jsx
@@ -1,16 +1,25 @@
 import "./index.css"
 import TunnelListItem from "../TunnelListItem/index.jsx";
+import {useEffect, useState} from "react";
 
-export default function TunnelList({list, serviceIp}) {
-    return (
-        <div>
-            {
-                list?.map(v => {
-                    return (
-                        <TunnelListItem serviceIp={serviceIp} key={v.port} item={v}/>
-                    )
-                })
-            }
-        </div>
-    )
+export default function TunnelList({list, serviceIp, onChecked}) {
+    const [checked, setChecked] = useState(0);
+
+    useEffect(() => {
+        if (list && list.length > 0) {
+            setChecked(list[0].id)
+            if (onChecked) onChecked(list[0])
+        }
+    }, [list])
+
+    return (<div>
+        {list?.map(v => {
+            return (<div onClick={() => {
+                setChecked(v.id)
+                if (onChecked) onChecked(v);
+            }}>
+                <TunnelListItem checked={checked === v.id} serviceIp={serviceIp} key={v.id} item={v}/>
+            </div>)
+        })}
+    </div>)
 }
