6f051fd1c49c8d543df21dbd97b4910b6a5ea4e8
heath <duweihengmr@163.com>
2023-11-21 17:36:28 +0800

    feat: /api/tunnel/edit api

--- src/api/tunnel.js
+++ src/api/tunnel.js
@@ -3,5 +3,10 @@ import Http from "../utils/api";
 export default {
     getTunnelList() {
         return Http.get("/api/tunnel/list");
+    },
+
+    editTunnel(param) {
+        console.log(param)
+        return Http.post("/api/tunnel/edit", param);
     }
 };
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -6,7 +6,7 @@ import Tunnel from "../../api/tunnel.js";
 import TunnelList from "../../components/TunnelList/index.jsx";
 import {useMsg} from "../../components/Msg/index.jsx";
 import {useSelector} from "react-redux";
-import {Empty, Tooltip} from "antd";
+import {Empty} from "antd";
 import {PlusOutlined, SyncOutlined} from "@ant-design/icons";
 import TextButton from "../../components/TextButton/index.jsx";
 
@@ -22,12 +22,38 @@ export default function Home() {
         getTunnelList();
     }, [])
 
+    const saveTunnelInfoChange = (name, value) => {
+        setCurrentTunnel({...currentTunnel, [name]: value});
+    };
+
     const getTunnelList = () => {
         Tunnel.getTunnelList()
             .then(res => {
                 if (res.code === 0) {
                     console.log(res)
                     setTunnels(res.data)
+                } else {
+                    Msg.onErrorMsg(res.msg)
+                }
+            })
+            .catch(e => {
+                Msg.onErrorMsg(e.message)
+            })
+    }
+
+    const onEditTunnel = () => {
+        Tunnel.editTunnel({
+            id: currentTunnel.id,
+            type: currentTunnel.mode,
+            client_id: currentTunnel.client.id,
+            port: currentTunnel.port,
+            remark: currentTunnel.remark,
+            target: currentTunnel.target.targetStr,
+        })
+            .then(res => {
+                if (res.code === 0) {
+                    Msg.onSucceedMsg("修改成功~")
+                    getTunnelList()
                 } else {
                     Msg.onErrorMsg(res.msg)
                 }
@@ -56,7 +82,7 @@ export default function Home() {
                 </div>
                 {!tunnels.length && <Empty style={{marginTop: 80}} description="暂无隧道..." image="./empty.svg"/>}
                 <div style={{overflowY: 'scroll', height: 360}}>
-                    {tunnels.length && <TunnelList
+                    {tunnels.length > 0 && <TunnelList
                         onChecked={(v) => setCurrentTunnel(v)}
                         list={tunnels}
                         serviceIp={userDate.serviceIp}
@@ -67,18 +93,46 @@ export default function Home() {
         {currentTunnel && <div className="right-container">
             <div style={{margin: "15px 20px"}}>
                 <div style={{display: "flex"}}>
-                    <BorderButton style={{marginRight: 12}}>保存</BorderButton>
+                    <BorderButton
+                        onClick={onEditTunnel}
+                        style={{marginRight: 12}}
+                    >
+                        保存
+                    </BorderButton>
                     <BorderButton>删除</BorderButton>
                 </div>
                 <div style={{marginTop: 10}}>
                     <LabelInput value={userDate.serviceIp}>外网IP：</LabelInput>
                     <LabelInput value={currentTunnel.port}>外网端口：</LabelInput>
                     <LabelInput value={currentTunnel.mode}>类型:</LabelInput>
-                    <LabelInput value={currentTunnel.remark} readOnly={false}>服务名称：</LabelInput>
-                    <LabelInput value={currentTunnel.target.targetStr.split(':')[0]}
-                                readOnly={false}>内网IP：</LabelInput>
-                    <LabelInput value={currentTunnel.target.targetStr.split(':')[1]}
-                                readOnly={false}>内网端口：</LabelInput>
+                    <LabelInput
+                        value={currentTunnel.remark} readOnly={false}
+                        onChange={(v) => {
+                            saveTunnelInfoChange("remark", v)
+                        }}
+                    >
+                        服务名称：
+                    </LabelInput>
+                    <LabelInput
+                        value={currentTunnel.target.targetStr.split(':')[0]}
+                        readOnly={false}
+                        onChange={(v) => {
+                            currentTunnel.target.targetStr = v + ':' + currentTunnel.target.targetStr.split(':')[1]
+                            saveTunnelInfoChange("target", currentTunnel.target)
+                        }}
+                    >
+                        内网IP：
+                    </LabelInput>
+                    <LabelInput
+                        value={currentTunnel.target.targetStr.split(':')[1]}
+                        readOnly={false}
+                        onChange={(v) => {
+                            currentTunnel.target.targetStr = currentTunnel.target.targetStr.split(':')[0] + ':' + v
+                            saveTunnelInfoChange("target", currentTunnel.target)
+                        }}
+                    >
+                        内网端口：
+                    </LabelInput>
                 </div>
             </div>
         </div>}
