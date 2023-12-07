9f5f4d495d9d058831295874ead6dfc5810b9d12
heath <duweihengmr@163.com>
2023-12-07 19:30:02 +0800

    feat: /api/user/client/status

--- src/api/user.js
+++ src/api/user.js
@@ -3,5 +3,8 @@ import Http from "../utils/api";
 export default {
     login(param) {
         return Http.post("/api/user/login", param);
+    },
+    clientStatus() {
+        return Http.post("/api/user/client/status");
     }
 };
